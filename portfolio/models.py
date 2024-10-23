7be02b3057c13cb22ef23e15b3416e86ffa199cd
Mohamad anas fattoum <samaanas30@gmail.com>
2024-10-23 22:24:41 +0200

    update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    Benutzername = models.CharField(max_length=50)
+    E_Mail = models.TextField(max_length=50)
+    Profilbild = models.ImageField(upload_to ='Profilbild', null=True, blank=True)
+
+
